1311c2078f4a5462232ba633aa5fa7123f841449
Maksim Iakovenko <yakovenkomax@protonmail.com>
2024-09-01 17:41:09 +0200

    Fix not found log error

--- src/utils/readJson.ts
+++ src/utils/readJson.ts
@@ -1,12 +1,8 @@
 import * as fs from 'fs';
 
 export const readJson = (path: string) => {
-  try {
-    const sourceData = fs.readFileSync(path, { encoding: 'utf-8' });
-    const data = JSON.parse(sourceData);
+  const sourceData = fs.readFileSync(path, { encoding: 'utf-8' });
+  const data = JSON.parse(sourceData);
 
-    return data;
-  } catch (e) {
-    return {};
-  }
+  return data;
 };
