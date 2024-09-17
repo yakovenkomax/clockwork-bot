196c155689b52c94f2498e0dbaedee01f04e1d77
Maksim Iakovenko <yakovenkomax@protonmail.com>
2024-09-17 19:19:47 +0200

    Improve image prompt

--- src/learn/getImage.ts
+++ src/learn/getImage.ts
@@ -17,12 +17,11 @@ export const getImage = async (dictionary: Dictionary) => {
   const entryFirstSentences = Object.keys(dictionary).map(word => dictionary[word][0].sentenceEN);
   const description = entryFirstSentences.join(' ');
 
-  const mainPart = `Create a square image in "${styleOfTheDay}" style described by these sentences: ${description}.`;
-  const whiteEdgePart = 'The the image must have a thin white edge around it.';
-  const noCrowdsPart = 'The image should not contain any crowds of people unless they are needed to show the described scene.';
+  const mainPart = `Create a square image in "${styleOfTheDay}" style described by these sentences: ${description}.\n\n`;
+  const noCrowdsPart = 'The image should not contain crowds of people.';
   const noCroppingPart = 'The contents of the image must fit in the square without cropping.';
 
-  const prompt = [mainPart, whiteEdgePart, noCrowdsPart, noCroppingPart].join('');
+  const prompt = [mainPart, noCrowdsPart, noCroppingPart].join('');
 
   return requestImage(prompt);
 };
